b88420fc99ee157852ac3c105a91b4b2d78f6c14
ASTER18K <donhon05@gmail.com>
2021-06-22 18:14:20 +0900

    0622 - 3

--- 04.Function.kt
+++ 04.Function.kt
@@ -18,9 +18,23 @@ fun plus(first : Int, second: Int): Int {
     println("--------")
     println("")
     println("Result")
-    val result : Int = first + second
-    return result
+    val a : Int = first + second
+    return a
 }
+
+// - 디폴트 값을 갖는 함수 만들기
+fun plusFive(first : Int, second : Int = 5): Int {
+    val b : Int = first + second
+    return b
+}
+
+
+// - 반환값이 없는 함수 만들기
+fun printPlus(first : Int, second : Int): Unit {
+    val c : Int = first + second
+    println(c)
+}
+
 
 fun main(array: Array<String>){
     // 함수를 호출하는 방법
@@ -29,12 +43,29 @@ fun main(array: Array<String>){
     println(result)
     // 인수를 명시적으로 전달하는 방법
 
+    // result = plus(5+10)
+
+    // result2 = plus(20+30)
 
     val result2 = plus(first = 20, second = 30)
     println(result2)
 
     val result3 = plus(second = 100, first = 10)
     println(result3)
-}
 
-//7분 50초...
+
+
+
+    // 디폴트 값을 갖는 함수 호출하기
+    println()
+    println()
+    println()
+    println("default Line")
+    val result4 = plusFive(10, 20)
+    println("--result4--")
+    println(result4)
+    val result5 = plusFive(10)
+    println("--result5--")
+    println(result5)
+
+}
