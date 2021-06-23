be6c0dd4edfe01a639479e4721495880ee989f82
ASTER18K <donhon05@gmail.com>
2021-06-23 18:24:52 +0900

    0623-1
    08.Controlflow

--- 07.OperatorPractice.kt
+++ 07.OperatorPractice.kt
@@ -5,7 +5,7 @@ fun main(array: Array<String>) {
     // 산술 연산자
     var a = 10 + 1
     var b = 10 - 1
-    var c = 10 * 1
+    var c = 1 * 9
     var d = 20 / 3
     var e = 20 % 3
 
@@ -26,7 +26,7 @@ fun main(array: Array<String>) {
     d /= 4
     e %= 2
     println()
-    println("Check Operator Line")
+    println("대입연산자 Line")
     println(a)
     println(b)
     println(c)
@@ -34,4 +34,32 @@ fun main(array: Array<String>) {
     println(e)
 
 
+    // 증감 연산자
+    a += 1
+    a++
+    b--
+    println()
+    println("증감 연산자 Line")
+    println(a)
+    println(b)
+
+    // 비교 연산자
+    var g = a > b
+    var h = a == b
+    var i = !h
+    var l = i != h
+    println()
+    println("T or F?")
+    println(g)
+    println(h)
+    println(i)
+    println(l)
+
+    // 논리 연산자
+    var j = h && i
+    var k = h || i
+    println()
+    println("논리")
+    println(j)
+    println(k)
 }
---
+++ 08.ControlFlow.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.Kotlin
+
+// 08. 제어흐름
+// - if, else
+
+fun main(args: Array<String>) {
+    val a: Int = 5
+    val b: Int = 10
+
+    if (a > b) {
+
+    } else {
+
+    }
+}
